ffd964a9791ce169bc97b8d720df765fa93e9464
PanYibin <panyibin@PanYibindeMacBook-Pro.local>
2015-01-22 23:42:16 +0800

    Initial Commit

---
+++ XMLTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMLTest
+//
+//  Created by PanYibin on 15/1/22.
+//  Copyright (c) 2015年 PanYibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMLTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XMLTest
+//
+//  Created by PanYibin on 15/1/22.
+//  Copyright (c) 2015年 PanYibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
